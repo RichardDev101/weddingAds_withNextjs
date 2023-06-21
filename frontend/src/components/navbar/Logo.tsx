8bc14cec6f2d91c9618708dd1b1be657862308c1
richard <dev.projects101@gmail.com>
2023-06-21 20:43:54 +0200

    Navigation bar layout and css has been implemented

--- frontend/src/components/navbar/Logo.tsx
+++ frontend/src/components/navbar/Logo.tsx
@@ -1,14 +1,13 @@
 "use client"
 
 import Image from "next/image";
-import {useRouter} from "next/navigation";
 
 import React from 'react';
 
 
 export default function Logo() {
 
-    const router = useRouter()
+
     return (
         <Image  alt="Logo"
                 className="hidden md:block cursor-pointer"
